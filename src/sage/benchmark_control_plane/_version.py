3711b032ee59f1b4edf8f0a21156bdfa194a8edc
zhang shuhao <shuhao_zhang@hust.edu.cn>
2026-02-26 12:02:14 +0800

    cd /home/shuhao/sagellm-control-plane-benchmark && GIT_EDITOR=true git cherry-pick --continue y cd /home/shuhao/sagellm-control-plane-benchmark && git status --short --branch chore(deps): sync pyproject internal dependency versions to latest PyPI

--- src/sage/benchmark_control_plane/_version.py
+++ src/sage/benchmark_control_plane/_version.py
@@ -1,4 +1,4 @@
 """Version information for sage-control-plane-benchmark."""
-__version__ = "0.1.0.0"
+__version__ = "0.2.0.3"
 __author__ = "IntelliStream Team"
 __email__ = "shuhao_zhang@hust.edu.cn"
