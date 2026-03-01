b9cbf2ca809a80ca84f99db5c7246fbbbddc6dc8
shuhao zhang <shuhao_zhang@hust.edu.cn>
2026-03-01 21:53:48 +0800

    chore: bump version to 0.2.0.7

--- src/sage/benchmark_control_plane/_version.py
+++ src/sage/benchmark_control_plane/_version.py
@@ -1,4 +1,4 @@
 """Version information for sage-control-plane-benchmark."""
-__version__ = "0.2.0.6"
+__version__ = "0.2.0.7"
 __author__ = "IntelliStream Team"
 __email__ = "shuhao_zhang@hust.edu.cn"
